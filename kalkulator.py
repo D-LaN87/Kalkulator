619f2976dbc665e5e15fabb1ccbbca1e15d79627
D-LaN87 <141350950+D-LaN87@users.noreply.github.com>
2025-05-21 13:46:30 +0200

    Updating calculator with def functions

--- kalkulator.py
+++ kalkulator.py
@@ -2,25 +2,42 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-print("Wybierz jaką operację chcesz wykonać: 1 Mnożenie, 2 Dzielenie, 3 Dodawanie, 4 Odejmowanie:")
-operation = input()
-
-a = float(input("Podaj pierwszą liczbę: "))
-b = float(input("Podaj drugą liczbę: "))
-if operation == '1':
+def multiply(a, b):
     logging.info(f"Działanie to {a:.2f} * {b:.2f}")
-    result = a * b
-elif operation == '2':
-    logging.info(f"Działanie to {b:.2f} : {a:.2f}")
-    result = a / b
-elif operation == '3':
-    logging.info(f"Działanie to {a:.2f} + {b:.2f}")
-    result = a + b
-elif operation == '4':
-    logging.info(f"Działanie to {a:.2f} - {b:.2f}")
-    result = a - b
-else:
-    print("Niepoprawny wybór")
-    exit()
+    return a * b
 
-print(f"Wynik to {result:.2f}")
+def divide(a, b):
+    logging.info(f"Działanie to {b:.2f} : {a:.2f}")
+    return a / b
+
+def add(a, b):
+    logging.info(f"Działanie to {a:.2f} + {b:.2f}")
+    return a + b
+
+def subtract(a, b):
+    logging.info(f"Działanie to {a:.2f} - {b:.2f}")
+    return a - b
+
+def what_operation():
+    print("Wybierz jaką operację chcesz wykonać:\n1 Mnożenie, 2 Dzielenie, 3 Dodawanie, 4 Odejmowanie:")
+    return input()
+
+def program():
+    operation = what_operation()
+    a = float(input("Podaj pierwszą liczbę: "))
+    b = float(input("Podaj drugą liczbę: "))
+    if operation == '1':
+        result = multiply(a, b)
+    elif operation == '2':
+        result = divide(a, b)
+    elif operation == '3':
+        result = add(a, b)
+    elif operation == '4':
+        result = subtract(a, b)
+    else:
+        print("Niepoprawny wybór operacji")
+        exit()
+
+    print(f"Wynik to {result:.2f}")
+    
+program()
